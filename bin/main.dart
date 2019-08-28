052d4909be51c97458012eb6065bc0f4701c3a4e
Igoru <fuzzyqu@protonmail.com>
2019-08-27 23:10:08 -0400

    Initial commit

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:lalg2/lalg2.dart' as lalg2;
+
+main(List<String> arguments) {
+  print('Hello world: ${lalg2.calculate()}!');
+}
---
+++ test/lalg2_test.dart
@@ -0,0 +1,8 @@
+import 'package:lalg2/lalg2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
